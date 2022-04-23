c753bc267a7e3512ece8142b03eced2c0dbe3fe5
Gloria Gu <zg92@duke.edu>
2022-04-23 17:09:25 -0400

    Update

--- add_fun_people.py
+++ add_fun_people.py
@@ -1,13 +1,18 @@
+"""Application functions for People object"""
+
 from db import conn
 from people import People
+from utils import clear_entries
+from tkinter import ttk
 
 
-def add_person(name_entry, username_entry, phone_entry):
+def add_person(
+    name_entry: ttk.Entry, username_entry: ttk.Entry, phone_entry: ttk.Entry
+):
     name = name_entry.get()
     username = username_entry.get()
     phone = phone_entry.get()
 
-    # TODO: check for missed info
     if name == "" or username == "" or phone == "":
         print("you missed some information; plz try again")
         clear_entries([name_entry, username_entry, phone_entry])
@@ -19,11 +24,39 @@ def add_person(name_entry, username_entry, phone_entry):
         clear_entries([name_entry, username_entry, phone_entry])
         return
 
-    people = People(name=name, username=username, phone=phone)
+    person = People(name=name, username=username, phone=phone)
 
     # clear text in entries
     clear_entries([name_entry, username_entry, phone_entry])
-    print(person_key, "added")
+    print(person._person_key, "added")
 
     conn.incr("PID", 1)
+    return
+
+
+def del_person(person_key_entry: ttk.Entry):
+    person_key = person_key_entry.get()
+    if not conn.sismember("people", person_key):
+        print(person_key, "is not in the library")
+        person_key_entry.delete(0, "end")
+        return
+
+    name = conn.hget(person_key, "name").decode("utf-8")
+    conn.srem(name, person_key)
+    username = conn.hget(person_key, "username").decode("utf-8")
+    conn.srem(username, person_key)
+    phone = conn.hget(person_key, "phone").decode("utf-8")
+    conn.srem(phone, person_key)
+
+    # return all the books and expire the person_book_set
+    person_book_set_key = person_key + ":books"
+    for book_key in conn.smembers(person_book_set_key):
+        conn.hset(book_key, "isAvailable", 1)
+    conn.expire(person_book_set_key, 0)
+
+    conn.delete(person_key)
+    conn.srem("people", person_key)
+
+    print(person_key, "deleted")
+    person_key_entry.delete(0, "end")
     return
--- people.py
+++ people.py
@@ -7,12 +7,33 @@ from db import conn
 class People:
     def __init__(self, name: str, username: str, phone: int) -> None:
         self._book = []
-        person_key = "person:" + conn.get("PID").decode("utf-8")
-        conn.hset(person_key, "name", name)
-        conn.hset(person_key, "username", username)
-        conn.hset(person_key, "phone", phone)
+        self._person_key = "person:" + conn.get("PID").decode("utf-8")
+        conn.hset(self._person_key, "name", name)
+        conn.hset(self._person_key, "username", username)
+        conn.hset(self._person_key, "phone", phone)
 
-        conn.sadd("people", person_key)
-        conn.sadd(name, person_key)
-        conn.sadd(username, person_key)
-        conn.sadd(phone, person_key)
+        conn.sadd("people", self._person_key)
+        conn.sadd(name, self._person_key)
+        conn.sadd(username, self._person_key)
+        conn.sadd(phone, self._person_key)
+
+    def __del__(self):
+        """Delete person"""
+        name = conn.hget(self._person_key, "name").decode("utf-8")
+        conn.srem(name, self._person_key)
+        username = conn.hget(self._person_key, "username").decode("utf-8")
+        conn.srem(username, self._person_key)
+        phone = conn.hget(self._person_key, "phone").decode("utf-8")
+        conn.srem(phone, self._person_key)
+
+    @property
+    def person_key(self):
+        """Getter of self._person_key"""
+        return self._person_key
+
+
+if __name__ == "__main__":
+    person = People("ok", "okk", 1234)
+    person2 = People("ok2", "okk2", 12342)
+    del person2
+    print(conn.hgetall("person:2"))
